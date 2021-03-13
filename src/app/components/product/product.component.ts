c4316e3ef6e000ef6c3fdcaa397da4038a9dd24e
sedanurgulocak <sedanurgulocak@gmail.com>
2021-03-14 00:09:53 +0300

    ✨ The scenario to list products by category has been added

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { Product } from 'src/app/models/product';
 import { ProductService } from 'src/app/services/product.service';
 
@@ -13,10 +14,17 @@ export class ProductComponent implements OnInit {
   dataLoaded = false;
 
   
-  constructor(private productService: ProductService) { }
+  constructor(private productService: ProductService, private activatedRoute:ActivatedRoute) { }
 
   ngOnInit(): void {
-    this.getProducts();
+    this.activatedRoute.params.subscribe(params=>{
+      if(params["categoryId"]){
+        this.getProductsByCategory(params["categoryId"])
+      }else{
+        this.getProducts()
+      }
+    })
+    
   }
 
   getProducts(){
@@ -24,7 +32,12 @@ export class ProductComponent implements OnInit {
       this.products = response.data;
       this.dataLoaded = true;
     })
-
   }
 
+  getProductsByCategory(categoryId:number){
+    this.productService.getProductsByCategory(categoryId).subscribe(response => {
+      this.products = response.data;
+      this.dataLoaded = true;
+    })
+  }
 }
