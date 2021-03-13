472c31edc9ad561e712348a812707fe801b8428a
sedanurgulocak <sedanurgulocak@gmail.com>
2021-03-13 21:55:28 +0300

    🔨 Add getCategories

--- src/app/components/category/category.component.ts
+++ src/app/components/category/category.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Category } from 'src/app/models/category';
+import { CategoryService } from 'src/app/services/category.service';
 
 @Component({
   selector: 'app-category',
@@ -9,9 +10,17 @@ import { Category } from 'src/app/models/category';
 export class CategoryComponent implements OnInit {
 
   categories: Category[]=[];
-  constructor() { }
+  constructor(private categoryService:CategoryService) { }
 
   ngOnInit(): void {
+    this.getCategories();
+  }
+
+  getCategories(){
+    this.categoryService.getCategories().subscribe(response => {
+      this.categories = response.data;
+    })
+
   }
 
 }
