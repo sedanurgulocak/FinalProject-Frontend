bf3e6b010646c6490130cdd4d4438d2089db5d83
sedanurgulocak <sedanurgulocak@gmail.com>
2021-03-13 21:41:59 +0300

    🔨 Add category service operations

--- src/app/services/category.service.ts
+++ src/app/services/category.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http'
+import { Observable } from 'rxjs';
+import { ListResponseModel } from '../models/ListResponseModel';
+import { Category } from '../models/category';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CategoryService {
 
-  constructor() { }
+  apiUrl = "https://localhost:44379/api/categories/getall"
+  constructor(private httpClient:HttpClient) { }
+
+  getCategories():Observable<ListResponseModel<Category>> {
+    return this.httpClient.get<ListResponseModel<Category>>(this.apiUrl);
+  }
 }
